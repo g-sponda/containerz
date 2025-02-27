3543dbe768b3b834eedd711c7aae7ff03f5cfd4e
g-sponda <13527982+g-sponda@users.noreply.github.com>
2025-02-28 00:39:07 +0100

    chore: create const for dir, create function to build args sent to child; create function to work with cgroups v2, and functions to decide which version to us e

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,12 @@ const c = @cImport({
 });
 const std = @import("std");
 const linux = std.os.linux;
+const dir = std.fs.cwd();
 
 pub fn main() !void {
     // Get allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // call deinit to free if if necessary
+    // call deinit to free if necessary
     defer _ = gpa.deinit();
     const allocator = gpa.allocator(); // use allocator
 
@@ -42,14 +43,16 @@ pub fn main() !void {
 
 // Create a container and run <cmd> inside it.
 fn run(args: [][]u8) !void {
-    std.debug.print("Run execution.\n\tRunning command: {s} args: {s}", .{ args[0], args[1..] });
+    std.debug.print("Run execution.\n\tRunning command: {s} args: {s}\n", .{ args[0], args[1..] });
     // Get allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // call deinit to free if if necessary
+    // call deinit to free if necessary
     defer _ = gpa.deinit();
     const allocator = gpa.allocator(); // use allocator
 
-    var proc = std.process.Child.init(args, allocator);
+    const child_args = try build_child_args(args);
+    std.debug.print("Child args: {s}\n", .{child_args});
+    var proc = std.process.Child.init(child_args, allocator);
     proc.stdin = std.io.getStdIn();
     proc.stdout = std.io.getStdOut();
     proc.stderr = std.io.getStdErr();
@@ -57,18 +60,32 @@ fn run(args: [][]u8) !void {
     try isolate();
     try setup_user_ns();
 
-    _ = try proc.spawnAndWait(); // start child process and wait
+    try proc.spawn(); // start child process and wait
+    _ = try proc.wait();
 }
 
-// create
+fn build_child_args(args: [][]u8) ![][]u8 {
+    const arrayllocator = std.heap.page_allocator;
+    var child_args = std.ArrayList([]u8).init(arrayllocator);
+    defer child_args.deinit();
+
+    try child_args.append(@constCast("/proc/self/exe"));
+    try child_args.append(@constCast("child"));
+    for (args) |arg| {
+        try (child_args.append(arg));
+    }
+    return try child_args.toOwnedSlice();
+}
+
+// Setup the container and run the command specified as argument
 fn child(args: [][]u8) !void {
-    std.debug.print("Child execution.\nRunning command: {s} args: {s}", .{ args[0], args[1..] });
+    std.debug.print("Child execution.\n\tRunning command: {s} args: {s}\n", .{ args[0], args[1..] });
 
     defer cleanup();
 
     try set_hostname();
     try setup_fs();
-    try setup_Cgroups();
+    try setup_cgroups_v2();
     try exec_cmd(args);
 }
 
@@ -81,7 +98,6 @@ fn isolate() !void {
 }
 
 fn set_hostname() !void {
-    // try std.posix.sethostname("containerz");
     const container_hostname = "containerz";
     const result = c.sethostname(container_hostname.ptr, container_hostname.len);
 
@@ -98,8 +114,8 @@ fn set_hostname() !void {
 }
 
 fn setup_fs() !void {
-    // Change the root fs
-    _ = linux.chroot("/home/containerz");
+    // Change the root fylesystem to the one created beforehand at the defined value on the chroot bellow
+    _ = linux.chroot("/home/containerz/genericfs");
     _ = linux.chdir("/");
 
     // Mount new procfs inside the container
@@ -111,36 +127,84 @@ fn setup_fs() !void {
 // disable setgroups before modifying user/group mapping
 fn setup_user_ns() !void {
     // Require for setting UID mapping
-    try std.fs.cwd().writeFile(.{ .sub_path = "/proc/self/setgroups", .data = "deny" });
-    try std.fs.cwd().writeFile(.{ .sub_path = "/proc/self/uid_map", .data = "0 1000 1" });
-    try std.fs.cwd().writeFile(.{ .sub_path = "/proc/self/gid_map", .data = "0 1000 1" });
+    try dir.writeFile(.{ .sub_path = "/proc/self/setgroups", .data = "deny" });
+    try dir.writeFile(.{ .sub_path = "/proc/self/uid_map", .data = "0 1000 1" });
+    try dir.writeFile(.{ .sub_path = "/proc/self/gid_map", .data = "0 1000 1" });
+}
+
+fn is_cgroups_v2() bool {
+    const path_cgroup_file_cgroup_v2 = "/sys/fs/cgroup/cgroup.controllers";
+
+    // check if the call didn't returned error, so it was a success and the file exists,
+    // this file is a file that exists on cgroup v2, so if it exists we now the cgroup is the v2,
+    // otherwise if we get an error it might be v1
+    dir.access(path_cgroup_file_cgroup_v2, .{}) catch return false;
+
+    return true;
+}
+
+fn setup_cgroups() !void {
+    if (is_cgroups_v2()) {
+        std.debug.print("cgroups V2", .{});
+        try setup_cgroups_v2();
+    } else {
+        std.debug.print("cgroups V1", .{});
+        try setup_cgroups_v1();
+    }
 }
 
 // Configure Cgroups to limit resources, and protect the container from
 // for example a fork bomb
-fn setup_Cgroups() !void {
+fn setup_cgroups_v1() !void {
     const cgroup_path = "/sys/fs/cgroup/pids/containerz";
 
     // Create Cgroup
-    _ = linux.mkdir(cgroup_path, 0o755);
+    try dir.makePath(cgroup_path);
 
     // limit the number of processes to 20
-    try std.fs.cwd().writeFile(.{ .sub_path = cgroup_path ++ "/pids.max", .data = "50" });
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/pids.max", .data = "50" });
 
     // automatically remove the cgroup when empty
-    try std.fs.cwd().writeFile(.{ .sub_path = cgroup_path ++ "/notify_on_release", .data = "1" });
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/notify_on_release", .data = "1" });
 
     // add the current process to the cgroup
     var pid_buf: [10]u8 = undefined;
     const pid_str = try std.fmt.bufPrint(&pid_buf, "{}", .{linux.getpid()});
 
-    try std.fs.cwd().writeFile(.{ .sub_path = cgroup_path ++ "/cgroup.procs", .data = pid_str });
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/cgroup.procs", .data = pid_str });
+}
+
+fn setup_cgroups_v2() !void {
+    const cgroup_path = "/sys/fs/cgroup/containerz";
+
+    // Create the cgroup directory
+    try dir.makePath(cgroup_path);
+    //TODO: fix: `thread 1 panic: reached unreachable code`
+    var cgroup_dir = try dir.openDir(cgroup_path, .{ .iterate = true });
+    defer cgroup_dir.close();
+
+    // Change ownership to allow non-root access (optional, but safer)
+    const uid = 1000; // Change to your user ID
+    const gid = 1000; // Change to your group ID
+    try cgroup_dir.chown(uid, gid);
+    std.debug.print("Chown gaven", .{});
+
+    // Enable the "pids" controller
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/cgroup.subtree_control", .data = "+pids" });
+
+    // Limit process count
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/pids.max", .data = "50" });
+
+    // Add current process
+    var pid_buf: [10]u8 = undefined;
+    const pid_str = try std.fmt.bufPrint(&pid_buf, "{}", .{linux.getpid()});
+    try dir.writeFile(.{ .sub_path = cgroup_path ++ "/cgroup.procs", .data = pid_str });
 }
 
 fn exec_cmd(args: [][]u8) !void {
     // Get allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    // call deinit to free if if necessary
+    // call deinit to free if necessary
     defer _ = gpa.deinit();
     const allocator = gpa.allocator(); // use allocator
 
